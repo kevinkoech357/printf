098eaf4697223b1abe347a8a204f81427d3e3466
Korir007 <danielkorir303@gmail.com>
2023-03-30 22:05:07 +0300

    changes

--- main.h
+++ main.h
@@ -25,5 +25,10 @@ int print_binary(va_list list, char buffer[], int flags, int width, int precisio
 int print_char(va_list list, char buffer[], int flags, int width, int precision, int size);
 int print_string(va_list list, char buffer[], int flags, int width, int precision, int size);
 int print_percent(va_list list, char buffer[]);
+int print_unsigned(va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_octal(va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_hexa_upper(va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_hexa(va_list list, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 #endif /* MAIN_H */
