9252c6adaa9fe6909a889b6c5bcda47913bf47f2
Kevin Koech <kevinkoech@proton.me>
2023-03-30 20:22:14 +0300

    added handlers for size, flags, width and precision

--- print_char.c
+++ print_char.c
@@ -2,14 +2,19 @@
 
 /**
 * print_char - prints a char
-* @list: list of arguments
+* @list: list a of arguments
 * @buffer: buffer array to handle print
+* @flags:  calculates active flags
+* @width: width
+* @precision: precision specification
+* @size: size specifier
 * Return: number of chars printed
 */
 
-int print_char(va_list types, char buffer[],)
+int print_char(va_list list, char buffer[],
+int flags, int width, int precision, int size)
 {
 	char c = va_arg(list, int);
-
-	return (handle_write_char(c, buffer, size));
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
+
