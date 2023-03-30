2b33a15fd56e1f79c034033d09c21d3246baabbf
Kevin Koech <kevinkoech@proton.me>
2023-03-30 20:17:45 +0300

    added a function that prints hexadecimal number in lower or upper

--- func1.c
+++ func1.c
@@ -111,3 +111,49 @@ int print_hexa_upper(va_list list, char buffer[],
 		flags, 'X', width, precision, size));
 }
 
+/**
+ * print_hexa - prints a hexadecimal number in lower or upper
+ * @list: List of arguments
+ * @map_to: array of values to map the number to
+ * @buffer: buffer array to handle print
+ * @flags:  calculates active flags
+ * @flag_ch: calculates active flags
+ * @width: get width
+ * @precision: precision specification
+ * @size: size specifier
+ * @size: size specification
+ * Return: number of chars printed
+ */
+
+int print_hexa(va_list list, char map_to[], char buffer[],
+	int flags, char flag_ch, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(list, unsigned long int);
+	unsigned long int init_num = num;
+
+	UNUSED(width);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = map_to[num % 16];
+		num /= 16;
+	}
+
+	if (flags & F_HASH && init_num != 0)
+	{
+		buffer[i--] = flag_ch;
+		buffer[i--] = '0';
+	}
+
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
