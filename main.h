0b52c6f3d8eff63d74837d1f54664bd7c16138ee
Korir007 <danielkorir303@gmail.com>
2023-03-30 21:35:33 +0300

    changes

--- main.h
+++ main.h
@@ -11,6 +11,12 @@
 /**FLAGS**/
 #define F_MINUS 1
 #define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/**SIZES**/
+
 
 int _printf(const char *format, ...);
 int _precision(const char *format, int *i, va_list list);
