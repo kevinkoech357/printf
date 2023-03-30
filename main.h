bfb57a6a2e8f43770e772fdae062b6c10b299528
Korir007 <danielkorir303@gmail.com>
2023-03-30 22:37:34 +0300

    print reverse

--- main.h
+++ main.h
@@ -32,6 +32,7 @@ int print_hexa_upper(va_list list, char buffer[], int flags, int width, int prec
 int print_hexa(va_list list, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
