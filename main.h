f393080f38f4fd40a3ef19506f66851c591c7215
Kevin Koech <kevinkoech@proton.me>
2023-03-31 08:17:49 +0300

    modified print percent prototype

--- main.h
+++ main.h
@@ -56,7 +56,7 @@ int print_int(va_list list, char buffer[], int flags, int width, int precision, 
 int print_binary(va_list list, char buffer[], int flags, int width, int precision, int size);
 int print_char(va_list list, char buffer[], int flags, int width, int precision, int size);
 int print_string(va_list list, char buffer[], int flags, int width, int precision, int size);
-int print_percent(va_list list, char buffer[]);
+int print_percent(va_list list, char buffer[],int flags, int width, int precision, int size);
 int print_unsigned(va_list list, char buffer[], int flags, int width, int precision, int size);
 int print_octal(va_list list, char buffer[], int flags, int width, int precision, int size);
 int print_hexadecimal(va_list list, char buffer[], int flags, int width, int precision, int size);
