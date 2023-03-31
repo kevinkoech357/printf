6eb0827682438b3df737169f6ea9b387a1e5cd1b
Kevin Koech <kevinkoech@proton.me>
2023-03-31 08:19:08 +0300

    works fine

--- print_percent.c
+++ print_percent.c
@@ -7,9 +7,16 @@
 * Return: int value
 */
 
-int print_percent(va_list list, char buffer[])
+int print_percent(va_list list, char buffer[],
+int flags, int width, int precision, int size)
 {
 	UNUSED(buffer);
+	UNUSED(list);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
 
 	return (write(1, "%%", 1));
 }
