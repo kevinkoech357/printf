707b99f445eed2dc0ac9d17a69f2194dbc762e5e
Kevin Koech <kevinkoech@proton.me>
2023-03-31 08:17:21 +0300

    spelling mistake correction

--- _printf.c
+++ _printf.c
@@ -15,19 +15,17 @@ void print_buffer(char buffer[], int *buff_index);
 
 int _printf(const char *format, ...)
 {
-	int i, printed, printed_chars;
-	int flags, width, precision, size, buff_ind = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_index = 0;
 
 	va_list list;
 
-	printed = printed_chars = 0;
-
 	char buffer[BUFF_SIZE];
 
-	int (format == NULL)
+	if (format == NULL)
 			return (-1);
 
-	va_start(list, format)
+	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
@@ -35,16 +33,15 @@ int _printf(const char *format, ...)
 		{
 			buffer[buff_index++] = format[i];
 			if (buff_index == BUFF_SIZE)
-			{
 				print_buffer(buffer, &buff_index);
-				printed_chars++;
-			}
+			printed_chars++;
+		}
 		else
 		{
 			print_buffer(buffer, &buff_index);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
+			precision = _precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
@@ -52,7 +49,6 @@ int _printf(const char *format, ...)
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
-			}
 		}
 	}
 	print_buffer(buffer, &buff_index);
